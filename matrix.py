b2f287ff714504c3e43159096821f38e6b8d7851
pineapple3770 <32078893+pineapple3770@users.noreply.github.com>
2017-10-03 16:07:44 -0700

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,1 +1,21 @@
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
+code = [ ]
+print('Code input type')
+default = input()
+if default == 'default':
+  code = ['1', '2']
+else:
+  for letter in alphabet:
+    print(letter + ':')
+    num = input()
+    code+= num
+print(code)
+lettercode = dict(zip(alphabet, code))
+print(lettercode)
+print('type secret message:')
+message = input()
+message = list(message.lower())
+nummessage = [ ]
+print(message)
+for character in message:
+  print(message[character - 1])
